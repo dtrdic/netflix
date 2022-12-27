1372b8a818b78b06928e57913ad0e179270776c8
dtrdic <dtrdic2@gmail.com>
2022-12-27 15:58:59 +0100

    cleaning

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB_ghL5L7bF2i3rLfVIUelylO_tPyxJJ9g",
+    authDomain: "netflix-2-689ee.firebaseapp.com",
+    projectId: "netflix-2-689ee",
+    storageBucket: "netflix-2-689ee.appspot.com",
+    messagingSenderId: "220796335499",
+    appId: "1:220796335499:web:4f61a11f6d5266cb2c2b8e"
+  };
