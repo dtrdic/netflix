72d4049cc6c7c625a0fc504e729bd0377292eb22
dtrdic <dtrdic2@gmail.com>
2022-12-30 10:46:42 +0100

    signIn screan styled

--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,7 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import "../css/LoginScreen.css";
+import SignInScreen from "./SignInScreen";
 
 function LoginScreen() {
+  const [signIn, setSignIn] = useState(false);
   return (
     <div className="loginScreen">
       <div className="loginScreen__background">
@@ -10,24 +12,35 @@ function LoginScreen() {
           src="https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png"
           alt=""
         ></img>
-        <button className="loginScreen__button">Sign in</button>
+        <button onClick={() => setSignIn(true)} className="loginScreen__button">
+          Sign in
+        </button>
         <div className="loginScreen__gradient" />
       </div>
       <div className="loginScreen__body">
-        <>
-          <h1>Unlimited films, Tv programs and more</h1>
-          <h2> Watch anywhere. Cancel any time</h2>
-          <h3>
-            Ready to watch? Enter your email to create or restart your
-            membership
-          </h3>
-          <div className="loginScreen__input">
-            <form>
-              <input type="email" placeholder="Email Address" />
-              <button className="loginScreen__getStarted">GET STARTED</button>
-            </form>
-          </div>
-        </>
+        {signIn ? (
+          <SignInScreen />
+        ) : (
+          <>
+            <h1>Unlimited films, Tv programs and more</h1>
+            <h2> Watch anywhere. Cancel any time</h2>
+            <h3>
+              Ready to watch? Enter your email to create or restart your
+              membership
+            </h3>
+            <div className="loginScreen__input">
+              <form>
+                <input type="email" placeholder="Email Address" />
+                <button
+                  onClick={() => setSignIn(true)}
+                  className="loginScreen__getStarted"
+                >
+                  GET STARTED
+                </button>
+              </form>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
