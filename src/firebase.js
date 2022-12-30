5b332a2184499b6b81f9a531be0ff05f6d95fffe
dtrdic <dtrdic2@gmail.com>
2022-12-30 11:52:28 +0100

    connected with firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,17 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
-    apiKey: "AIzaSyB_ghL5L7bF2i3rLfVIUelylO_tPyxJJ9g",
-    authDomain: "netflix-2-689ee.firebaseapp.com",
-    projectId: "netflix-2-689ee",
-    storageBucket: "netflix-2-689ee.appspot.com",
-    messagingSenderId: "220796335499",
-    appId: "1:220796335499:web:4f61a11f6d5266cb2c2b8e"
-  };
+  apiKey: "AIzaSyB_ghL5L7bF2i3rLfVIUelylO_tPyxJJ9g",
+  authDomain: "netflix-2-689ee.firebaseapp.com",
+  projectId: "netflix-2-689ee",
+  storageBucket: "netflix-2-689ee.appspot.com",
+  messagingSenderId: "220796335499",
+  appId: "1:220796335499:web:4f61a11f6d5266cb2c2b8e",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+
+export { auth };
+export default db;
